ad5014ebc5754d0952fc2f7ef667d6e6a7f91ef6
Zhalgas Zhaksylyk <ziqzaq2005@gmail.com>
2025-12-28 04:45:04 +0500

    commited

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print('Hello from Zhalgas Zhaksylyk')
+
+from dotenv import load_dotenv
+import os
+
+def print_author():
+    load_dotenv(dotenv_path=r'C:\Users\Zhalgas\Desktop\Новая папка\Яндекс Практикум\Git\1_learn\.env')
+    author = os.getenv('AUTHOR')
+    print(f'Author of project is :{author}')
+
+print(print_author())
